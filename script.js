08a8ecd947d3156a9f2f5fdc7852ffefaf357e31
Ylgner W <ylgner.wsantana@gmail.com>
2026-02-11 17:42:16 -0300

    melhorias

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 window.addEventListener("load", () => {
-    const video = document.createElement("video");
-    const divVideo = document.querySelector(".divVideo");
+  const video = document.createElement("video");
+  const divVideo = document.querySelector(".divVideo");
+  const fallbackImg = document.querySelector(".divVideo img");
 
+  video.src = "img/monstro.mp4";
+  video.autoplay = true;
+  video.muted = true;
+  video.playsInline = true;
+  video.loop = true;
 
-    video.src = "img/Flow.mp4";
-    video.autoplay = true;
-    video.loop = true;
-    video.muted = true;
-    video.playsInline = true;
+  video.style.opacity = 0
 
-    video.style.opacity = 0;
+  video.addEventListener("canplaythrough", ()=> {
+    video.style.opacity = 1
+    fallbackImg.style.display = "none";
+  });
 
-    video.addEventListener("canplaythrough", () => {
-        video.style.opacity = 1;
-    })
-
-    divVideo.appendChild(video);
+  divVideo.appendChild(video);
 });
-
